9dc703dbb21d9327b9b18a739eb99562f698a593
Dewald <dewald@perfectcircle.co.za>
2018-03-20 16:41:38 +0200

    fix

--- vendor/extensions/locations/config/routes.rb
+++ vendor/extensions/locations/config/routes.rb
@@ -1,31 +1,31 @@
 Refinery::Core::Engine.routes.append do
 
   # Frontend routes
-  # Refinery::Locations::Location.roots.each do |parent|
-  #   match "/#{parent.slug}" => "locations/locations#show"
-  #     if parent.accommodations.present?
-  #       parent.accommodations.each do |acc|
-  #         match "/#{parent.slug}/#{acc.slug}" => "accommodations/accommodations#show"
-  #       end
-  #     end
-  #     parent.children.each do |child|
-  #       match "/#{parent.slug}/#{child.slug}" => "locations/locations#show"
-  #       if child.accommodations.present?
-  #         child.accommodations.each do |acc|
-  #           match "/#{parent.slug}/#{child.slug}/#{acc.slug}" => "accommodations/accommodations#show"
-  #         end
-  #       end
-  #       child.children.each do |grand|
-  #        match "/#{parent.slug}/#{child.slug}/#{grand.slug}" => "locations/locations#show"
-  #
-  #         if grand.accommodations.present?
-  #           grand.accommodations.each do |acc|
-  #             match "/#{parent.slug}/#{child.slug}/#{grand.slug}/#{acc.slug}" => "accommodations/accommodations#show"
-  #           end
-  #         end
-  #       end
-  #   end
-  # end
+  Refinery::Locations::Location.roots.each do |parent|
+    match "/#{parent.slug}" => "locations/locations#show"
+      if parent.accommodations.present?
+        parent.accommodations.each do |acc|
+          match "/#{parent.slug}/#{acc.slug}" => "accommodations/accommodations#show"
+        end
+      end
+      parent.children.each do |child|
+        match "/#{parent.slug}/#{child.slug}" => "locations/locations#show"
+        if child.accommodations.present?
+          child.accommodations.each do |acc|
+            match "/#{parent.slug}/#{child.slug}/#{acc.slug}" => "accommodations/accommodations#show"
+          end
+        end
+        child.children.each do |grand|
+         match "/#{parent.slug}/#{child.slug}/#{grand.slug}" => "locations/locations#show"
+
+          if grand.accommodations.present?
+            grand.accommodations.each do |acc|
+              match "/#{parent.slug}/#{child.slug}/#{grand.slug}/#{acc.slug}" => "accommodations/accommodations#show"
+            end
+          end
+        end
+    end
+  end
 
   namespace :locations do
     resources :locations, :path => '', :only => [:index, :show]
