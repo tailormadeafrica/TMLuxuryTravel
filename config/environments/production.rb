52f4c0957380b57202e2745667580a7bafdc738d
Dewald <dewald@perfectcircle.co.za>
2018-07-12 16:05:51 +0200

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,10 @@ TailorMadeAfrica::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  config.assets.precompile += ['*.js', '*.json', '*.css', '*.scss', 'refinery/*.js', 'refinery/*.css']
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
