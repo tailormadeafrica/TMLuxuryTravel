5a101d755cae920e9223b3d86eae44651b7de385
Dewald <dewald@perfectcircle.co.za>
2018-09-06 10:35:03 +0200

    dokku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 if Rails.env.development?
- ENV["REDISTOGO_URL"] =  'redis://localhost:6379' if Rails.env.development?
+ ENV["REDIS_URL"] =  'redis://localhost:6379' if Rails.env.development?
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = {url: ENV["REDISTOGO_URL"]}
+  config.redis = {url: ENV["REDIS_URL"]}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {url: ENV["REDISTOGO_URL"]}
+  config.redis = {url: ENV["REDIS_URL"]}
 end
